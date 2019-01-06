9fdfdb099fea99c72e729c5d3165c7a18b106bd5
Your <janetsbird@gmail.com>
2019-01-06 14:38:08 -0500

    save after completion of hamburger menu

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 //initialize AOS
 AOS.init({
-    once: false, // whether animation should happen only once - while scrolling down
+    once: true, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
 });
 
 
-//hamburger menu
+//dropdown toggle menu
+jQuery(document).ready(function () {
+
+    jQuery('.toggleNav').click(function (e) {
+        jQuery(this).toggleClass('active');
+        jQuery('.mainNav ul').toggleClass('active');
+
+        e.preventDefault();
+    });
+
+});
