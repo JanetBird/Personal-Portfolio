f03301668b7c19f85af4041392ca94dbec072e1c
Your <janetsbird@gmail.com>
2019-01-06 13:09:55 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//initialize AOS
+AOS.init({
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+});
+
+
+//hamburger menu
